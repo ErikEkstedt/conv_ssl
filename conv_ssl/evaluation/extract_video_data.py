c201a82ccb7b02f80339b9311d6143f253c55d10
Erik <eeckee@gmail.com>
2022-04-04 13:37:37 +0200

    update video data extraction

--- conv_ssl/evaluation/extract_video_data.py
+++ conv_ssl/evaluation/extract_video_data.py
@@ -1,3 +1,6 @@
+from os import makedirs
+from os.path import join
+
 import torch
 from tqdm import tqdm
 from conv_ssl.model import VPModel
@@ -10,14 +13,76 @@ from datasets_turntaking import DialogAudioDM
 everything_deterministic()
 
 
+def video_data_single(
+    model,
+    dset,
+    idx,
+    audio_duration=10,
+    audio_overlap=5,
+    batch_size=8,
+    savepath="assets/video",
+):
+    """
+    Extract video data from single dialog (defined by `idx`)
+    """
+
+    # Extract all data for video
+    d = dset.get_dialog_sample(idx)
+    batches = dset.dialog_to_batch(
+        d,
+        audio_duration=audio_duration,
+        audio_overlap=audio_overlap,
+        batch_size=batch_size,
+    )
+
+    # Combine all data
+    start_frame = int(audio_overlap * dm.vad_hz)
+    start_sample = int(audio_overlap * dm.sample_rate)
+    video_data = {"waveform": [], "va": [], "vh": [], "p": [], "p_bc": [], "logits": []}
+    losses = []
+    for i, batch in enumerate(tqdm(batches)):
+        loss, out, probs, batch = model.output(batch)
+        losses.append(loss["total"])
+        tmp_batch_size = out["logits_vp"].shape[0]
+        start_batch = 0
+        if i == 0:
+            video_data["waveform"].append(batch["waveform"][0].to("cpu"))
+            video_data["va"].append(batch["vad"][0].to("cpu"))
+            video_data["vh"].append(batch["vad_history"][0].to("cpu"))
+            video_data["p"].append(probs["p"][0].to("cpu"))
+            video_data["p_bc"].append(probs["bc_prediction"][0].to("cpu"))
+            video_data["logits"].append(out["logits_vp"][0].to("cpu"))
+            start_batch = 1
+        for n in range(start_batch, tmp_batch_size):
+            video_data["waveform"].append(batch["waveform"][n, start_sample:].to("cpu"))
+            video_data["va"].append(batch["vad"][n, start_frame:].to("cpu"))
+            video_data["vh"].append(batch["vad_history"][n, start_frame:].to("cpu"))
+            video_data["p"].append(probs["p"][n, start_frame:].to("cpu"))
+            video_data["p_bc"].append(probs["bc_prediction"][n, start_frame:].to("cpu"))
+            video_data["logits"].append(out["logits_vp"][n, start_frame:].to("cpu"))
+
+    for name, vallist in video_data.items():
+        video_data[name] = torch.cat(vallist)
+
+    # Add additional info
+    video_data["loss"] = torch.stack(losses).mean()
+    video_data["vap_bins"] = model.VAP.vap_bins.cpu()
+    video_data["session"] = d["session"][0]
+
+    # save to disk
+    makedirs(savepath, exist_ok=True)
+
+    filename = join(savepath, f"{d['session'][0]}_video_data.pt")
+    torch.save(video_data, filename)
+    print("Saved -> ", filename)
+
+    return video_data
+
+
 if __name__ == "__main__":
 
     # Load model
-    # model_id = "120k8fdv"
-    # checkpoint_path = get_checkpoint(run_path=model_id)
-    # checkpoint_path = load_paper_versions(checkpoint_path)
-    checkpoint_path = "./artifacts/model-120k8fdv:v0/model_new.ckpt"
-
+    checkpoint_path = "assets/model.ckpt"
     model = VPModel.load_from_checkpoint(checkpoint_path, strict=False)
     model = model.eval()
     if torch.cuda.is_available():
@@ -45,46 +110,8 @@ if __name__ == "__main__":
     dm.prepare_data()
     dm.setup(None)
 
-    dset = dm.val_dset
-    # chunk dialog -> segments -> order in batch (to fit gpu)
-    audio_duration = 10
-    audio_overlap = 5
-    batch_size = 8
-    # Extract all data for video
-    d = dset.get_dialog_sample(0)
-    batches = dset.dialog_to_batch(
-        d,
-        audio_duration=audio_duration,
-        audio_overlap=audio_overlap,
-        batch_size=batch_size,
-    )
-    # Combine all data
-    start_frame = int(audio_overlap * dm.vad_hz)
-    start_sample = int(audio_overlap * dm.sample_rate)
-    video = {"waveform": [], "va": [], "vh": [], "p": [], "p_bc": [], "logits": []}
-    for i, batch in enumerate(tqdm(batches)):
-        loss, out, probs, batch = model.output(batch)
-        tmp_batch_size = out["logits_vp"].shape[0]
-        start_batch = 0
-        if i == 0:
-            video["waveform"].append(batch["waveform"][0].to("cpu"))
-            video["va"].append(batch["vad"][0].to("cpu"))
-            video["vh"].append(batch["vad_history"][0].to("cpu"))
-            video["p"].append(probs["p"][0].to("cpu"))
-            video["p_bc"].append(probs["bc_prediction"][0].to("cpu"))
-            video["logits"].append(out["logits_vp"][0].to("cpu"))
-            start_batch = 1
-        for n in range(start_batch, tmp_batch_size):
-            video["waveform"].append(batch["waveform"][n, start_sample:].to("cpu"))
-            video["va"].append(batch["vad"][n, start_frame:].to("cpu"))
-            video["vh"].append(batch["vad_history"][n, start_frame:].to("cpu"))
-            video["p"].append(probs["p"][n, start_frame:].to("cpu"))
-            video["p_bc"].append(probs["bc_prediction"][n, start_frame:].to("cpu"))
-            video["logits"].append(out["logits_vp"][n, start_frame:].to("cpu"))
-
-    for name, vallist in video.items():
-        video[name] = torch.cat(vallist)
-    video["vap_bins"] = model.VAP.vap_bins.cpu()
-
-    # save to disk
-    torch.save(video, "assets/video.pt")
+    # select 20 first test-videos
+    for idx in range(20):
+        video_data = video_data_single(
+            model, dset=dm.test_dset, idx=idx, savepath="assets/video/test"
+        )
