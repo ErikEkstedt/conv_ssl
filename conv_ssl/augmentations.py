fixed average duration augmentation and most of Figure plots

conv_ssl/augmentations.py
@@ -56,9 +56,9 @@ def praat_to_torch(sound):
     return torch.from_numpy(y)
 
 
-###############################################
-###################### F0 #####################
-###############################################
+############################################
+################### F0 #####################
+############################################
 def create_flat_pitch_tier(manipulation, target_f0: float, start: float, end: float):
     """Flat pitch tier"""
     pitch_tier = call(manipulation, "Create PitchTier", "flat", start, end)
@@ -255,6 +255,9 @@ def shift_pitch_batch(
     return flat
 
 
+############################################
+################### Low-Pass ###############
+############################################
 def low_pass_filter_resample(x, cutoff_freq, sample_rate=16000):
     new_freq = int(cutoff_freq * 2)  # Nyquist
     x = AF.resample(x, orig_freq=sample_rate, new_freq=new_freq)
@@ -274,6 +277,9 @@ def smooth(x, kernel_size=15):
     return averager(x)[:, 0]
 
 
+############################################
+################### Intensity ##############
+############################################
 def intensity_mask(intensity, cutoff=0.2, kernel_size=15):
     ints = smooth(intensity, kernel_size)
     ints[ints < 0] = 0

conv_ssl/evaluation/phrase_dataset.py
@@ -1,5 +1,13 @@
 import torch
+import numpy as np
 from torch.utils.data import Dataset
+from os.path import join, basename
+import matplotlib.pyplot as plt
+import librosa
+from librosa import display
+
+from conv_ssl.augmentations import torch_to_praat_sound
+from conv_ssl.evaluation.duration import read_text_grid, EXAMPLE_TO_TARGET_WORD
 from conv_ssl.utils import read_json
 from datasets_turntaking.utils import (
     load_waveform,
@@ -7,6 +15,167 @@ from datasets_turntaking.utils import (
     time_to_frames,
 )
 from vap_turn_taking.utils import vad_list_to_onehot, get_activity_history
+
+
+TEXT_GRID_ROOT = "assets/phrases_beta/alignment"
+
+
+def audio_path_text_grid_path(path):
+    name = basename(path).replace(".wav", ".TextGrid")
+    return join(TEXT_GRID_ROOT, name)
+
+
+def words_to_vad(words):
+    vad_list = []
+    for s, e, w in words:
+        vad_list.append([s, e])
+    return [vad_list, []]
+
+
+def plot_sample_data(sample, sample_rate=16000, ax=None, fontsize=12, plot=False):
+    snd = torch_to_praat_sound(sample["waveform"], sample_rate)
+    pitch = snd.to_pitch()
+    pitch_values = pitch.selected_array["frequency"]
+    pitch_values[pitch_values == 0] = np.nan
+    xmin, xmax = snd.xs().min(), snd.xs().max()
+    melspec = librosa.power_to_db(
+        librosa.feature.melspectrogram(
+            y=sample["waveform"].numpy(), sr=sample_rate, n_fft=800, hop_length=160
+        ),
+        ref=np.max,
+    )[0]
+
+    fig = None
+    if ax is None:
+        fig, ax = plt.subplots(3, 1, figsize=(6, 5))
+
+    # Waveform
+    ax[0].plot(snd.xs(), snd.values.T, alpha=0.4)
+    ax[0].set_xlim([xmin, xmax])
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    ax[0].set_ylabel("waveform", fontsize=fontsize)
+
+    target_word = EXAMPLE_TO_TARGET_WORD[sample["example"]]
+    # bounds = get_word_times(target_word, sample)
+    # end_time = bounds[0][1]
+
+    # Plot text on top of waveform
+    SCP_line_x = None
+    y_min = -0.8
+    y_max = 0.8
+    diff = y_max - y_min
+    steps = 4
+    for ii, (start_time, end_time, word) in enumerate(sample["words"]):
+        yy = y_min + diff * (ii % steps) / steps
+        mid = start_time + 0.5 * (end_time - start_time)
+
+        # previous word was SCP
+        c = "k"
+        llw = 1
+        if sample["words"][ii - 1][-1] == target_word and sample["size"] == "long":
+            c = "r"
+            llw = 2
+            SCP_line_x = start_time
+
+        ax[0].text(
+            x=mid,
+            y=yy,
+            s=word,
+            fontsize=12,
+            horizontalalignment="center",
+        )
+        ax[0].vlines(
+            start_time,
+            ymin=-1,
+            ymax=1,
+            linestyle="dashed",
+            linewidth=llw,
+            color=c,
+            alpha=0.8,
+        )
+
+    # final end of word
+    ax[0].vlines(
+        sample["words"][-1][1],
+        ymin=-1,
+        ymax=1,
+        # linestyle="dashed",
+        linewidth=2,
+        color="r",
+        alpha=0.8,
+    )
+
+    # SPEC
+    img = librosa.display.specshow(
+        melspec,
+        x_axis="time",
+        y_axis="mel",
+        sr=sample_rate,
+        hop_length=160,
+        fmax=8000,
+        ax=ax[1],
+    )
+    ymin, ymax = ax[1].get_ylim()
+    ax[1].vlines(
+        sample["words"][-1][1],
+        ymin=ymin,
+        ymax=ymax,
+        # linestyle="dashed",
+        linewidth=2,
+        color="r",
+    )
+    if SCP_line_x is not None:
+        ax[1].vlines(
+            SCP_line_x,
+            ymin=ymin,
+            ymax=ymax,
+            linestyle="dashed",
+            linewidth=2,
+            color="r",
+        )
+    ax[1].set_xticks([])
+    ax[1].set_xlabel("")
+    ax[1].set_ylabel("Mel (Hz)", fontsize=fontsize)
+    ax[1].yaxis.tick_right()
+
+    # Pitch
+    ax[2].plot(pitch.xs(), pitch_values, "o", markersize=3, color="b")
+    ax[2].set_xlim([xmin, xmax])
+    ax[2].set_xticks([])
+    ymin, ymax = ax[2].get_ylim()
+    if ymax - ymin < 10:
+        ymin -= 5
+        ymax += 5
+        ax[2].set_ylim([ymin, ymax])
+    ymin, ymax = ax[2].get_ylim()
+    ax[2].vlines(
+        sample["words"][-1][1],
+        ymin=ymin,
+        ymax=ymax,
+        # linestyle="dashed",
+        linewidth=2,
+        color="r",
+    )
+    if SCP_line_x is not None:
+        ax[2].vlines(
+            SCP_line_x,
+            ymin=ymin,
+            ymax=ymax,
+            linestyle="dashed",
+            linewidth=2,
+            color="r",
+        )
+    ax[2].yaxis.tick_right()
+    ax[2].set_ylabel("F0 (Hz)", fontsize=fontsize)
+
+    plt.subplots_adjust(
+        left=0.1, bottom=0.05, right=0.9, top=0.95, wspace=None, hspace=0.09
+    )
+
+    if plot:
+        plt.pause(0.1)
+    return fig, ax, SCP_line_x
 
 
 class PhraseDataset(Dataset):
@@ -60,10 +229,6 @@ class PhraseDataset(Dataset):
                 for gender, sample_list in vv.items():
                     for ii in range(len(sample_list)):
                         indices.append([example, long_short, gender, ii])
-        # for example, gender_dict in self.data.items():
-        #     for gender, sample_list in gender_dict.items():
-        #         for ii in range(len(sample_list)):
-        #             indices.append([example, gender, ii])
         return indices
 
     def __repr__(self):
@@ -88,8 +253,16 @@ class PhraseDataset(Dataset):
     def __len__(self):
         return len(self.indices)
 
-    def get_sample_data(self, b, example):
-        """Get the sample from the dialog"""
+    def _sample_data(self, b, vad_list):
+        """
+        Get the sample from the dialog
+
+        Returns dict containing:
+            waveform,
+            dataset_name,
+            vad,
+            vad_history
+        """
         # Loads the dialog waveform (stereo) and normalize/to-mono for each
         # smaller segment in loop below
         waveform, _ = load_waveform(
@@ -103,9 +276,7 @@ class PhraseDataset(Dataset):
         ret = {
             "waveform": waveform,
             "dataset_name": "phrases",
-            "session": f'{example}_{b["gender"]}_{b["tts"]}',
         }
-        ret.update(b)
 
         # VAD-frame of relevant part
         if self.vad:
@@ -113,7 +284,7 @@ class PhraseDataset(Dataset):
             start_frame = 0
             end_frame = time_to_frames(duration, self.vad_hop_time)
             all_vad_frames = vad_list_to_onehot(
-                b["vad"],
+                vad_list,
                 hop_time=self.vad_hop_time,
                 duration=duration,
                 channel_last=True,
@@ -145,16 +316,24 @@ class PhraseDataset(Dataset):
             ret["vad"] = all_vad_frames[
                 start_frame : end_frame + self.vad_horizon
             ].unsqueeze(0)
+        return ret
 
-        ##############################################
-        # Include words/starts/size if included
-        ##############################################
-        if "words" in b:
-            ret["words"] = [b["words"]]
-        if "starts" in b:
-            ret["starts"] = [b["starts"]]
-        if "size" in b:
-            ret["size"] = [b["size"]]
+    def get_sample_data(self, sample, example):
+        tg = read_text_grid(audio_path_text_grid_path(sample["audio_path"]))
+        vad_list = words_to_vad(tg["words"])
+        # Returns: waveform, dataset_name, vad, vad_history
+        ret = self._sample_data(sample, vad_list)
+        ret["example"] = example
+        ret["words"] = tg["words"]
+        ret["phones"] = tg["phones"]
+
+        for k, v in sample.items():
+            if k in ["vad", "words"]:
+                continue
+            ret[k] = v
+        # ret["gender"] = sample["gender"]
+        # ret["size"] = sample["size"]
+        # ret["text"] = sample["text"]
         return ret
 
     def get_sample(self, example, long_short, gender, id):
@@ -163,12 +342,16 @@ class PhraseDataset(Dataset):
 
     def __getitem__(self, idx):
         example, long_short, gender, nidx = self.indices[idx]
-        sample = self.data[example][long_short][gender][nidx]
-        return self.get_sample_data(sample, example)
+        return self.get_sample(example, long_short, gender, nidx)
 
 
 if __name__ == "__main__":
+
     dset = PhraseDataset("assets/phrases_beta/phrases.json")
     sample = dset.get_sample("student", "short", "female", 0)
     for k, v in sample.items():
         print(f"{k}: {type(v)}")
+
+    sample = dset.get_sample("student", "long", "female", 2)
+    fig, ax, scp_line = plot_sample_data(sample)
+    plt.show()