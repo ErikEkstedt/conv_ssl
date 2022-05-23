dbf42e9b58d35ab1c6fa21a6f341d96bf163e59f
Erik <eeckee@gmail.com>
2022-05-23 12:41:44 +0200

    script sync

--- scripts/test_augmentation.bash
+++ scripts/test_augmentation.bash
@@ -16,47 +16,48 @@ cpc100=$chpath"cpc_48_100hz_3mkvq5fk.ckpt"
 # $aug_eval $swb $cpc50 +augmentation='flat_intensity' +max_batches=10
 # $aug_eval $swb $cpc50 +augmentation='only_f0' +max_batches=10
 
-# Augmentations: low_pass (only_f0), flat_f0, flat_intensity, equal_duration
-# $aug_eval $swb $cpc20 +augmentation='flat_f0'
-# $aug_eval $swb $cpc20 +augmentation='shift_f0'
-# $aug_eval $swb $cpc20 +augmentation='flat_intensity'
-# $aug_eval $swb $cpc20 +augmentation='only_f0'
-# $aug_eval $fisher $cpc20 +augmentation='flat_f0'
-# $aug_eval $fisher $cpc20 +augmentation='shift_f0'
-# $aug_eval $fisher $cpc20 +augmentation='flat_intensity'
-# $aug_eval $fisher $cpc20 +augmentation='only_f0'
-#
-# $aug_eval $swb $cpc50 +augmentation='flat_f0'
-# $aug_eval $swb $cpc50 +augmentation='shift_f0'
-# $aug_eval $swb $cpc50 +augmentation='flat_intensity'
-# $aug_eval $swb $cpc50 +augmentation='only_f0'
-# $aug_eval $fisher $cpc50 +augmentation='flat_f0'
-# $aug_eval $fisher $cpc50 +augmentation='shift_f0'
-# $aug_eval $fisher $cpc50 +augmentation='flat_intensity'
-# $aug_eval $fisher $cpc50 +augmentation='only_f0'
-#
-# $aug_eval $swb $cpc100 +augmentation='flat_f0'
-# $aug_eval $swb $cpc100 +augmentation='shift_f0'
-# $aug_eval $swb $cpc100 +augmentation='flat_intensity'
-# $aug_eval $swb $cpc100 +augmentation='only_f0'
-# $aug_eval $fisher $cpc100 +augmentation='flat_f0'
-# $aug_eval $fisher $cpc100 +augmentation='shift_f0'
-# $aug_eval $fisher $cpc100 +augmentation='flat_intensity'
-# $aug_eval $fisher $cpc100 +augmentation='only_f0'
 
 
 # both last
 $aug_eval $both $cpc20 +augmentation='flat_f0'
 $aug_eval $both $cpc20 +augmentation='shift_f0'
 $aug_eval $both $cpc20 +augmentation='only_f0'
-# $aug_eval $both $cpc20 +augmentation='flat_intensity'
+$aug_eval $both $cpc20 +augmentation='flat_intensity'
 
 $aug_eval $both $cpc50 +augmentation='flat_f0'
 $aug_eval $both $cpc50 +augmentation='shift_f0'
 $aug_eval $both $cpc50 +augmentation='only_f0'
-# $aug_eval $both $cpc50 +augmentation='flat_intensity'
+$aug_eval $both $cpc50 +augmentation='flat_intensity'
 
 $aug_eval $both $cpc100 +augmentation='flat_f0'
 $aug_eval $both $cpc100 +augmentation='shift_f0'
 $aug_eval $both $cpc100 +augmentation='only_f0'
-# $aug_eval $both $cpc100 +augmentation='flat_intensity'
+$aug_eval $both $cpc100 +augmentation='flat_intensity'
+
+# Augmentations: low_pass (only_f0), flat_f0, flat_intensity, equal_duration
+$aug_eval $swb $cpc20 +augmentation='flat_f0'
+$aug_eval $swb $cpc20 +augmentation='shift_f0'
+$aug_eval $swb $cpc20 +augmentation='flat_intensity'
+$aug_eval $swb $cpc20 +augmentation='only_f0'
+$aug_eval $fisher $cpc20 +augmentation='flat_f0'
+$aug_eval $fisher $cpc20 +augmentation='shift_f0'
+$aug_eval $fisher $cpc20 +augmentation='flat_intensity'
+$aug_eval $fisher $cpc20 +augmentation='only_f0'
+
+$aug_eval $swb $cpc50 +augmentation='flat_f0'
+$aug_eval $swb $cpc50 +augmentation='shift_f0'
+$aug_eval $swb $cpc50 +augmentation='flat_intensity'
+$aug_eval $swb $cpc50 +augmentation='only_f0'
+$aug_eval $fisher $cpc50 +augmentation='flat_f0'
+$aug_eval $fisher $cpc50 +augmentation='shift_f0'
+$aug_eval $fisher $cpc50 +augmentation='flat_intensity'
+$aug_eval $fisher $cpc50 +augmentation='only_f0'
+
+$aug_eval $swb $cpc100 +augmentation='flat_f0'
+$aug_eval $swb $cpc100 +augmentation='shift_f0'
+$aug_eval $swb $cpc100 +augmentation='flat_intensity'
+$aug_eval $swb $cpc100 +augmentation='only_f0'
+$aug_eval $fisher $cpc100 +augmentation='flat_f0'
+$aug_eval $fisher $cpc100 +augmentation='shift_f0'
+$aug_eval $fisher $cpc100 +augmentation='flat_intensity'
+$aug_eval $fisher $cpc100 +augmentation='only_f0'
